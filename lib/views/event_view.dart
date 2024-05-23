2754184312edc1bf49dba2b7714f7f3db3635ed6
JSM80 <mail@moellers-hule.dk>
2024-05-23 12:30:48 +0200

    JSM-Event01

---
+++ lib/views/event_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'login_view.dart';
+
+class EventView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Current Events'),
+      );
+
+
+    }
+}
